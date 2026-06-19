e1f25fb8d332699d882aafbb1f6e552cdae91875
Giuseppe Cera <gcera177@gmail.com>
2026-06-19 14:21:19 +0200

    #15 Starting to create the structure of the Average Age page

--- client/src/pages/dashboard/AvgAge.jsx
+++ client/src/pages/dashboard/AvgAge.jsx
@@ -3,8 +3,48 @@ import styles from "../../style/dashboard/avgage.module.css";
 
 export const AvgAge = () => {
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Età media lampioni</h1>
-        </div>
+            <div className={styles.container}>
+                <h1 style={styles.title}>Overview Età Media</h1>
+                <div className={styles.overviewElements}>
+                    <div className={styles.leftElements}>
+
+                    <div className={ `${styles.overviewElement} ${styles.active}`}>
+                        <p>Età Media complessiva: 8.7yr</p>
+                    </div>
+                    <div className={ `${styles.overviewElement} ${styles.inactive}`}>
+                        <p>Sostituzioni Recenti</p>
+                    </div>
+                    </div>
+                    <div className={styles.rightElements}>
+                            <h3>Lampioni da Cambiare</h3>
+                            
+                        <ul className={styles.oldLampsList}>
+                            <li><p>Lampione 1</p><p></p></li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                            <li>Lampione 1</li>
+                        </ul>
+                    
+                    </div>
+    
+                </div>
+    
+            </div>
     )
 }
